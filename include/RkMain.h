2f5e56cde4870b2d1ee938cfe58e35e591859cae
Iurie Nistor <nistor@iurie.org>
2019-01-30 06:44:14 -0500

    RkMain class

---
+++ include/RkMain.h
@@ -0,0 +1,11 @@
+#ifndef RK_MAIN_H
+
+class RkMain {
+  public:
+          RkMain()
+          RkMain(int argc, char **argv);
+          ~RkMain();
+          int exec();
+};
+
+#endif // RK_MAIN_H
---
+++ src/RkMain.cpp
@@ -0,0 +1,19 @@
+#include "RkMain.cpp"
+
+RkMain::RkMain()
+{
+}
+
+RkMain::RkMain(int argc, char **argv)
+{
+       RK_UNUSED(argc);
+       RK_UNUSED(argv);
+}
+
+RkMain::~RkMain()
+{
+}
+
+int RkMain::exec()
+{
+}
