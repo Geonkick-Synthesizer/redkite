692a4428e77c87b8387670dc262df2c1d77c3646
Iurie Nistor <nistor@iurie.org>
2019-06-04 11:25:08 +0100

    update version

--- include/Rk.h
+++ include/Rk.h
@@ -24,10 +24,10 @@
 #ifndef RK_GLOBAL_H
 #define RK_GLOBAL_H
 
-#define RK_VERSION 0x000502
+#define RK_VERSION 0x000600
 #define RK_MAJOR   0x00
-#define RK_MINOR   0x05
-#define RK_PATCH   0x02
+#define RK_MINOR   0x06
+#define RK_PATCH   0x00
 
 #include <memory>
 #include <vector>
